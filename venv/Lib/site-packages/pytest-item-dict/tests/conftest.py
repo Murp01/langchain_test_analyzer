48ba5f1092ec8b53623edb71041c0ffca2bd9d89
Murp01 <pmurphy35@hotmail.com>
2025-03-10 00:34:54 +0000

    removed venv

--- venv/Lib/site-packages/pytest-item-dict/tests/conftest.py
+++
@@ -1,6 +0,0 @@
-#############################################
-#	Dual License: BSD-3-Clause AND MPL-2.0	#
-#	Copyright (c) 2024, Adam Nogowski		#
-#############################################
-
-pytest_plugins = 'pytester'
